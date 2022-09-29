73d91c477db7dde8f836633292e6babc766a8ca2
jepwebbe <94107611+jepwebbe@users.noreply.github.com>
2022-09-29 14:48:53 +0200

    thursday day end

---
+++ src/Components/Partials/Header.Styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+
+nav{
+        display: flex;
+        justify-content: space-between;
+        padding: 0 5rem;
+        height: 3.5rem;
+        width: 80vw;
+        margin: 0 auto;
+}
+`
